70cf127bf43059ff70146e380466cefcff42c0e5
Maureen <maureenwanjiku85@gmail.com>
2023-10-18 18:51:47 +0300

    add questions and answers in the js file

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "Which is the largest continent in the world?",
+        answers:[
+            {text: "Africa", correct: false},
+            {text: "Asia", correct: true},
+            {text: "Europe", correct: false},
+            {text: "Australia", correct: false},
+
+        ]
+    },
+    {
+        question: "Which of the following programming languages is not an OOP?",
+        answers: [
+            {text: "Ruby", correct: false},
+            {text: "java", correct: false},
+            {text: "C", correct: true},
+            {text: "javacsript", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following  is not a characteristic of an OOPL?",
+        answers: [
+            {text: "Polymorphism", correct: false},
+            {text: "Attachment", correct: true},
+            {text: "Inheritance", correct: false},
+            {text: "Abstraction", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following  is not a programming language?",
+        answers: [
+            {text: "PHP", correct: false},
+            {text: "C++", correct: false},
+            {text: "Ruby", correct: false},
+            {text: "Bootstrap", correct: true},
+        ]
+    },
+];
